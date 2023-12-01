ccc7fb1a4260d87017f5105e4ae39d22cf0e8402
2022171 <146198931+2022171@users.noreply.github.com>
2023-12-01 22:43:24 +0000

    Updated Java Staff class
    attributes were given, created getters and setters and constructors

--- src/revenuetaxcalculator/Staff.java
+++ src/revenuetaxcalculator/Staff.java
@@ -8,6 +8,116 @@ package revenuetaxcalculator;
  *
  * @author danie
  */
-public class Staff {
+public abstract class Staff {
+    int StaffID;
+    int phoneNumber;
+    String firstName;
+    String lastName;
+    String birthDate;
+    String email;
+    String Address;
+    String startDate;
+    String endDate;
+    String position;
+    String status;
+    String ppsN;
+    String iban;
+    String payFrequency;
+    private static int currentID = 1;
+    
+    public Staff(int StaffID, int phoneNumber, String firstName, String lastName, String birthDate, String email, String Address, String startDate, String endDate, 
+            String position, String status, String ppsN, String iban, String payFrequency) {
+        this.StaffID = currentID;
+        this.phoneNumber = phoneNumber;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.birthDate = birthDate;
+        this.email = email;
+        this.Address = Address;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.position = position;
+        this.status = status;
+        this.ppsN = ppsN;
+        this.iban = iban;
+        this.payFrequency = payFrequency;
+        currentID++;
+    }
+   
+    
+
+    public int getStaffID() {
+        return StaffID;
+    }
+
+    public int getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getBirthDate() {
+        return birthDate;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getAddress() {
+        return Address;
+    }
+
+    public String getStartDate() {
+        return startDate;
+    }
+
+    public String getEndDate() {
+        return endDate;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public String getPpsN() {
+        return ppsN;
+    }
+
+    public String getIban() {
+        return iban;
+    }
+
+    public String getPayFrequency() {
+        return payFrequency;
+    }
+
+    public static int getCurrentID() {
+        return currentID;
+    }
+    
+    public void setStaffID(int StaffID) {
+        this.StaffID = StaffID;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+    
+
     
 }
