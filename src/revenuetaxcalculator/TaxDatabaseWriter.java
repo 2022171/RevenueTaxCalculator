86c4f90c41f204c8e2b9c00414092ec6c156c88c
C2022188 <2022188@student.cct.ie>
2023-12-05 15:34:43 +0000

    Class TaxDatabaseWriter
    Setup the database writer

--- src/revenuetaxcalculator/TaxDatabaseWriter.java
+++ src/revenuetaxcalculator/TaxDatabaseWriter.java
@@ -4,11 +4,32 @@
  */
 package revenuetaxcalculator;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.List;
+
 /**
  *
  * @author danie
  */
-public class TaxDatabaseWriter {
+public class TaxDatabaseWriter extends DataBaseTax {
     
-    
+    public boolean addPatient (Staff staff) throws SQLException{
+         try(
+                Connection conn = DriverManager.getConnection(DB_URL, USER, PASSWORD);
+                Statement stmt = conn.createStatement();
+            ){
+             String sql = String.format("INSERT INTO " + TABLE_NAME + " VALUES ("
+                     + "'%s', '%s', '%s', %d);",
+                    staff.getStaffID(), staff.getPhoneNumber(), staff.getFirstName(), staff.getLastName(), staff.getBirthDate(), staff.getEmail(), staff.getAddress(), staff.getStartDate(), staff.getEndDate(), staff.getPosition(), staff.getStatus(), staff.getPpsN(), staff.getIban(), staff.getPayFrequency());
+                     stmt.execute(sql);
+                     return true;
+                           
+         }
+    }
+    public boolean addallStaff(List<Staff> allStaff){
+        return true;
+    }
 }
